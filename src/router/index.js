de8cbf45a48522b3f7261525bd92458de1e5ad1f
Morgan Murrah <mmurrah@airbridge.ac.nz>
2017-05-28 20:27:44 -0400

    Succesful basic migration to Vue-CLI [PWA template feat Webpack and more](http://vuejs-templates.github.io/webpack/structure.html)

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import myPhone from '@/components/myPhone'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'myPhone',
+      component: myPhone
+    }
+  ]
+})
