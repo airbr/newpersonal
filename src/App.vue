88b98248871146fa12dc23e8530a3280a847ed89
Morgan Murrah <mmurrah@airbridge.ac.nz>
2017-05-29 10:23:55 -0400

    Fonts & Title name

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,11 @@ body {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Helvetica', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+
 }
 
 main {
