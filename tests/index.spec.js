bf9ab5078b578415f949af0bc9421e8a3469719b
Morgan Murrah <morgan@wompmobile.com>
2022-06-04 23:45:13 -0700

    Test push

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -23,30 +23,4 @@ test('github link works', async ({ page }) => {
   const name = page.locator(".vcard-fullname");
   await expect(name).toHaveCount(1);
   await expect(name).toHaveText('Morgan Murrah');
-});
-
-
-// This doesn't work but was a worthy attempt:
-
-// test('amp validator', async ({ page }) => {
-//   // await page.goto('https://search.google.com/test/amp');
-//   await page.goto('https://www.google.com/search?q=amp+testing&oq=amp+test');
-//   await page.focus('input[name="urlnt"]');
-//   await page.fill('input[name="urlnt"]', 'https://morganwebdev.com');
-//   await page.locator('text="RUN TEST"').click(); 
-
-//   await page.focus('div div[role="button"]');
-//   // await page.locator('text="test url"').click(); 
-
-//   // await page.hover('text="test url"').click(); 
-
-//   // await page.press('div div[role="button"]', 'Enter');
-//   // const locator = page.locator('text="test url"');
-//   // ...
-
-//   // var rect = locator.getBoundingClientRect();
-//   // console.log(rect.top, rect.right, rect.bottom, rect.left);
-
-//   await page.screenshot({ path: 'screenshot.png' }); 
-// });
-
+}); 
