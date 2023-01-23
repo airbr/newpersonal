8328eca8f44ba80bdf9ada0a654b98693e012cc4
Morgan Murrah <morgan@morganwebdev.com>
2023-01-23 01:04:01 -0800

    $ npm run validate
    > morgan-murrah@6.1.0 validate
    > amphtml-validator src/index.html
    
    src/index.html: PASS

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,60 +1,76 @@
-// importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+/*
+Copyright 2015, 2019 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
-// AMP_SW.init({
-//     assetCachingOptions: [{
-//       regexp: /\.(png|jpg|woff2|woff|css|js)/,
-//       cachingStrategy: 'CACHE_FIRST',
-//     }],
-//     offlinePageOptions: {
-//       url: '/offline.html',
-//       assets: [],
-//     },
-//   });
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+// previously cached resources to be updated from the network.
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'offline.html';
 
-var CACHE_NAME = 'v3';
-
-var REQUIRED_FILES = [
-  'index.html',
-  'offline.html',
-  'coffee-crawl.html'
-];
-
-
-self.addEventListener('install', function(event) {
-
-  var offlineRequest = new Request('offline.html');
-
-  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-    console.log('[install] Caches opened, adding core components to cache');
-    return cache.addAll(REQUIRED_FILES);
-  }).then(function() {
-    console.log('[install] All resources cached, good to go');
-    return self.skipWaiting();
-  }));
-
-  event.waitUntil(fetch(offlineRequest).then(function(response) {
-    return caches.open('index').then(function(cache) {
-      console.log('[oninstall] Cached offline page', response.url);
-      return cache.put(offlineRequest, response);
-    });
-  }));
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[activate] Activating SW');
-  console.log('[activate] Claiming SW');
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
 
-  var request = event.request;
-  if (request.method === 'GET') {
-    event.respondWith(fetch(request).catch(function(error) {
-      console.error('[onfetch] Failed. Serving fallback: ', error);
-      return caches.open('offline').then(function(cache) {
-        return cache.match('offline.html');
-      });
-    }));
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
   }
+
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
 });
