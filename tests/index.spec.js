d8c827ae370a07b5ce08da2d3d45066a67106723
Morgan Murrah <morgan@morganwebdev.com>
2023-03-28 16:25:34 -0700

    Update Playwright

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,14 +1,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('headline exists', async ({ page }) => {
-  await page.goto('https://morganwebdev.com');
+  await page.goto('https://www.morganwebdev.com');
   const title = page.locator('.h-card p:nth-child(2)');
   await expect(title).toHaveCount(1);
   await expect(title).toHaveText('My name is Morgan and this is my website:');
 });
 
 test('16 icons and svgs are present', async ({ page }) => {
-  await page.goto('https://morganwebdev.com');
+  await page.goto('https://www.morganwebdev.com');
   const icons = page.locator('.icon');
   await expect(icons).toHaveCount(16);
   const svgs = page.locator('svg');
@@ -16,7 +16,7 @@ test('16 icons and svgs are present', async ({ page }) => {
 });
 
 test('github link works', async ({ page }) => {
-  await page.goto('https://morganwebdev.com');
+  await page.goto('https://www.morganwebdev.com');
   const github = page.locator("a[href='https://github.com/airbr']");
   await expect(github).toHaveCount(1);
   await github.click();
